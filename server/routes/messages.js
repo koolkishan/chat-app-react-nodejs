bb31f3ea7ff592625aeff2a72058a669f88a302f
Wahid Najim <133244826+Wahid-najim@users.noreply.github.com>
2024-08-06 17:16:50 +0600

    Update messages.js

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,7 +1,8 @@
+
 const { addMessage, getMessages } = require("../controllers/messageController");
 const router = require("express").Router();
 
-router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getMessages);
+router.post("/addmsg/",  addMessage);
+router.post("/getmsg/",  getMessages);
 
-module.exports = router;
+module.exports =  router;
